81b41f89cfb13ada9d2b0a4b8c781d784155a91f
ky077 <kylin077@gmail.com>
2025-10-17 15:14:48 +0800

    delete

--- TAA/js/footer.js
+++
@@ -1,9 +0,0 @@
-$(window).on('resize',function() {	
-	$('body').css('margin-bottom', $('.footer').height()); 
-});	
-$(document).ready(function() {
-    $(window).trigger('resize');
-});
-$(window).on('load',function() {
-    $(window).trigger('resize');
-});
--- TAA/js/list-icon.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$('a.list-icon').css('background-image', 'url(../images/icons8-copy-link-32.png)');
-	
-	$('a.list-icon[href$=".pdf"]').css('background-image', 'url(../images/icons8-pdf-32.png)');
-	$('a.list-icon[href$=".doc"], a.list-icon[href$=".docx"]').css('background-image', 'url(../images/icons8-word-32.png)');
-	$('a.list-icon[href$=".zip"]').css('background-image', 'url(../images/icons8-zip-32.png)');
-	$('a.list-icon[href$=".rar"]').css('background-image', 'url(../images/icons8-rar-32.png)');
-});
