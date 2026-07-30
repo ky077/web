fb15cd9474e35ff79333ad076fe8aa6d17d0a075
ky077 <kylin077@gmail.com>
2026-07-30 16:39:05 +0800

    PASSION AI

--- AI-English-Tutor/Content/js/main.js
+++ AI-English-Tutor/Content/js/main.js
@@ -6,5 +6,15 @@
 	
   //load header
   $('footer').load('master-footer.html');
+	
+	 //Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($('#spinner').length > 0) {
+        $('#spinner').removeClass('show');
+      }
+    }, 500);
+  };
+  spinner();
 
 })(jQuery);
