ff9eabfe7f2d5d28db1777fbeb05e95bcc68cb54
ky077 <kylin077@gmail.com>
2026-08-04 11:42:25 +0800

    2

--- AI-English-Tutor/Content/js/main.js
+++ AI-English-Tutor/Content/js/main.js
@@ -3,11 +3,11 @@
 
   //load header
   $('header').load('master-header.html');
-	
+
   //load header
   $('footer').load('master-footer.html');
-	
-	 //Spinner
+
+  //Spinner
   var spinner = function () {
     setTimeout(function () {
       if ($('#spinner').length > 0) {
@@ -16,5 +16,15 @@
     }, 500);
   };
   spinner();
+
+  //隱藏/顯示密碼
+  $('.btn-showHidePD').click(function () { 
+    var $button = $(this);
+    var $input = $button.siblings('input');
+
+    // 切換 icon 和 input 類型
+    $button.find('i').toggleClass('fa-eye-slash');
+    $input.attr('type', $input.attr('type') === 'password' ? 'text' : 'password');
+  });
 
 })(jQuery);
